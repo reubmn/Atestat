5c16209921004f26e566fd16a61d30e95cccb19a
reubmn <razvi.bm@gmail.com>
2025-08-24 17:59:31 +0300

    mk foldere pt stocare informatie

--- Source/main.py
+++ Source/main.py
@@ -1,6 +1,13 @@
 from PyQt5 import QtWidgets
 import interface
+import os
 import sys
+
+def setup_directories():
+    dirs = ['users', 'Subiecte', 'analytics']
+    for dir_name in dirs:
+        if not os.path.exists(dir_name):
+            os.makedirs(dir_name)
 
 app = QtWidgets.QApplication(sys.argv)
 
@@ -9,6 +16,7 @@ window = interface.MainWindow()
 window.setFixedWidth(800)
 window.setFixedHeight(800) 
 
+setup_directories()
 window.show()
 
 app.exec()
